9a4a26fe1f159d7784ada1825a76516f6bed57f6
saharsh <saharshg10@gmail.com>
2019-07-05 03:01:00 +0530

    optimized solution

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const transform = array => {
 function App() {
   const [output, setOutput] = React.useState([]);
   const [input, setInput] = React.useState([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 1, 0, 1],
+    [1, 1, 0, 0]
   ]);
 
   const toggle = (value, index, idx) => {
@@ -22,36 +22,63 @@ function App() {
   };
 
   const getOutput = () => {
-    const checkBound = idx => idx > -1 && idx < input.length;
-
-    const compute = (i, j) =>
-      0 +
-      (checkBound(i - 1) && checkBound(j - 1) && input[i - 1][j - 1] === 1) +
-      (checkBound(i - 1) && input[i - 1][j] === 1) +
-      (checkBound(i - 1) && checkBound(j + 1) && input[i - 1][j + 1] === 1) +
-      (checkBound(j - 1) && input[i][j - 1] === 1) +
-      (checkBound(j + 1) && input[i][j + 1] === 1) +
-      (checkBound(i + 1) && checkBound(j - 1) && input[i + 1][j - 1] === 1) +
-      (checkBound(i + 1) && input[i + 1][j] === 1) +
-      (checkBound(i + 1) && checkBound(j + 1) && input[i + 1][j + 1] === 1);
-
     let tOutput = [];
-    input.forEach((subArray, i) => {
-      subArray.forEach((item, j) => {
-        let value = 0;
-        if (item === 1) {
-          value = 9;
-        } else {
-          value = compute(i, j);
+    const tInput = input.flat();
+    tInput.forEach((item, i) => {
+      if (item === 1) {
+        tOutput[i] = 9;
+      } else {
+        let count = [];
+        if (i % 4 === 0) {
+          if (i + 1 < tInput.length) {
+            count.push(tInput[i + 1]);
+          }
+
+          if (i - 4 > -1) {
+            count.push(...tInput.slice(i - 4).slice(0, 2));
+          }
+          if (i + 4 < tInput.length) {
+            count.push(...tInput.slice(i + 4).slice(0, 2));
+          }
         }
-        tOutput.push(value);
-      });
+
+        if (i % 4 === 1 || i % 4 === 2) {
+          if (i - 1 > -1) {
+            count.push(tInput[i - 1]);
+          }
+          if (i + 1 < tInput.length) {
+            count.push(tInput[i + 1]);
+          }
+
+          if (i - 5 > -1) {
+            count.push(...tInput.slice(i - 5).slice(0, 3));
+          }
+          if (i + 3 < tInput.length) {
+            count.push(...tInput.slice(i + 3).slice(0, 3));
+          }
+        }
+
+        if (i % 4 === 3) {
+          if (i - 1 > -1) {
+            count.push(tInput[i - 1]);
+          }
+
+          if (i - 5 > -1) {
+            count.push(...tInput.slice(i - 5).slice(0, 2));
+          }
+          if (i + 3 < tInput.length) {
+            count.push(...tInput.slice(i + 3).slice(0, 2));
+          }
+        }
+        tOutput[i] = count.filter(it => it === 1).length;
+      }
     });
     setOutput(tOutput);
   };
 
   return (
     <div className="App">
+      Add mines:
       <table>
         <tbody>
           {input.map((item, index) => (
@@ -70,7 +97,10 @@ function App() {
           ))}
         </tbody>
       </table>
+      <br />
       <button onClick={getOutput}>Get output</button>
+      <br />
+      <br />
       <table>
         <tbody>
           {output.length > 0 &&
